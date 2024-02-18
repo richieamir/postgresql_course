d6f9dcbd1f389574369665aaa741b4b1e1cf4e60
Richie Rizal Amir <richieamir@gmail.com>
2024-02-18 12:55:38 +0700

    Basic Operations

---
+++ Basic Operation/Insert.sql
@@ -0,0 +1,4 @@
+-- Insert sample data into products table
+INSERT INTO products (name, price) VALUES ('Laptop', 1000.00);
+INSERT INTO products (name, price) VALUES ('Smartphone', 500.00);
+INSERT INTO products (name, price) VALUES ('Headphones', 100.00);
